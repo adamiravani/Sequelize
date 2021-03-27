e2eade9f58ffc37165fabadb50247aa695519e6b
adamiravani <78195922+adamiravani@users.noreply.github.com>
2021-03-27 19:06:24 -0400

    populate rows correctly

--- public/script.js
+++ public/script.js
@@ -1,32 +1,35 @@
-async function populateResturant(){
-    const diningRequest = await fetch('/api/dining');
-    const diningData = await diningRequest.json();
-    const targetBox= document.querySelector('.targetBox')
-    console.table(diningData)
+count = 0;
 
-    diningData.data.forEach((item) => {
-        const appendItem = document.createElement('div');
-        appendItem.classList.add('title', 'has-text-centered','is-parent','is-3')
-        appendItem.innerHTML = `
-        <article class="title is-child box has-background-link-dark">
-        <span class = "subtitle has-text-light has-text-weight-bold">
-        ${item.hall_name}</span>
-        <br />
-        <span class="has-text-light">
-        ${item.hall_address.split(',')[0]}
-        </span>
-        <br />
-        <span class="has-text-light">
-        ${item.hall_address.split(',')[1]}
-        </span>
-        </article>`;
-        targetBox.append(appendItem);
-    })
+async function populateResturant() {
+  const diningRequest = await fetch("/api/dining");
+  const diningData = await diningRequest.json();
+  console.table(diningData);
+  const name = document.querySelector(".name");
+  const address = document.querySelector(".address");
+
+  diningData.data.forEach((item) => {
+
+    const appendName = document.createElement('ul');
+    appendName.classList.add('title', 'has-text-centered','is-parent','is-3')
+    appendName.innerHTML = `
+    ${item.hall_name}
+    `;
+    name.append(appendName);
+
+    const appendAddress = document.createElement('ul');
+    appendAddress.classList.add('title', 'has-text-centered','is-parent','is-3')
+    appendAddress.innerHTML = `
+    ${item.hall_address.split(',')[0]}
+    `;
+    address.append(appendAddress);
+
+
+
+  });
 }
 
-
 async function windowActions() {
-    populateResturant();
+  populateResturant();
 }
 
 window.onload = windowActions();
